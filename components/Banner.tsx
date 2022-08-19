37d1d848e4e91188192545dec18f556eb52301eb
hansaeroy <65123417+hansaeroy@users.noreply.github.com>
2022-08-19 18:10:19 +0900

    1일차 수정

---
+++ components/Banner.tsx
@@ -0,0 +1,56 @@
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { baseUrl } from '../constants/movie';
+import { Movie } from '../typings';
+import { FaPlay } from 'react-icons/fa';
+import { InformationCircleIcon } from '@heroicons/react/solid';
+
+interface Props {
+  netflixOriginals: Movie[];
+}
+
+const Banner = ({ netflixOriginals }: Props) => {
+  const [movie, setMovie] = useState<Movie | null>(null);
+
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    );
+  }, [netflixOriginals]);
+
+  console.log(movie);
+  return (
+    <div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
+      <div className='absolute top-0 left-0 -z-10 h-[95vh] w-full'>
+        <Image
+          // next/image를 사용하여 이미지를 불러오기 위해선 반드시 "width" 와 "height" 값이나 "layout='fill'" 값을 사용해야 한다.
+          // 바로사용하면 next/image Un-configured Host 에러가 뜨는데
+          //
+          layout='fill'
+          src={`${baseUrl}${movie?.backdrop_path || movie?.poster_path}`}
+          objectFit='cover'
+        />
+      </div>
+      {/* 타이틀 */}
+      <h1 className='text-2xl lg:text-7xl md:text-4xl '>
+        {movie?.title || movie?.name || movie?.original_title}
+      </h1>
+      {/* 설명 */}
+      <p className='max-w-xs text-xs text-shadow-md md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl'>
+        {movie?.overview}
+      </p>
+
+      <div className='flex space-x-3'>
+        <button className='text-black bg-white bannerButton'>
+          <FaPlay className='w-4 h-4 text-black md:h-7 md:w-7' />
+          Play
+        </button>
+        <button className='bannerButton bg-[gray]/70'>
+          More Info <InformationCircleIcon className='w-5 h-5 md:h-8 md:w-8' />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
---
+++ components/Row.tsx
@@ -0,0 +1,67 @@
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
+import { useRef, useState } from 'react';
+import { Movie } from '../typings';
+import Thumbnail from './Thumbnail';
+
+interface Props {
+  title: string;
+  movies: Movie[];
+  //파이어베이스 사용할때
+  // movie: Movie | DocumentData[];
+}
+
+function Row({ title, movies }: Props) {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  //스크롤 이벤트
+  const handleClick = (direction: string) => {
+    setIsMoved(true);
+
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+
+      const scrollTo =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' });
+    }
+  };
+
+  // console.log(rowRef.current!.scrollLeft, rowRef.current!.clientWidth);
+  return (
+    <div className='h-40 space-y-0.5 md:space-y-2'>
+      <h2 className='w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl'>
+        {title}
+      </h2>
+      <div className='relative group md:-ml-2'>
+        <ChevronLeftIcon
+          className={`absolute top-0 bottom-0 z-40 m-auto transition opacity-0 cursor-pointer left-2 w-9 h-9 hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && 'hidden'
+          }`}
+          onClick={() => handleClick('left')}
+        />
+
+        {/* 썸네일 */}
+        <div
+          ref={rowRef}
+          className='flex items-center space-x-0.5 md:space-x-2.5 md:p-2 overflow-x-scroll scrollbar-hide
+        '
+        >
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
+
+        <ChevronRightIcon
+          className='absolute top-0 bottom-0 z-40 m-auto transition opacity-0 cursor-pointer right-2 w-9 h-9 hover:scale-125 group-hover:opacity-100'
+          onClick={() => handleClick('right')}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Row;
